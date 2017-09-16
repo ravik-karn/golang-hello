fb7b86dc932ddaecfccf901f8cc0fd44da43f8f8
Ravi-Kumar <ravik@thoughtworks.com>
2017-09-16 15:37:04 +0530

    Ravi | Add negroni middleware

--- main/hello.go
+++ main/hello.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"time"
-	"log"
-	"net/http"
-
 	"github.com/codegangsta/negroni"
 	"personal/hello/handler"
 )
@@ -13,13 +9,5 @@ func main() {
 	n := negroni.New()
 	router := handler.Router()
 	n.UseHandler(router)
-
-	s := &http.Server{
-		Addr:           ":5400",
-		Handler:        n,
-		ReadTimeout:    200 * time.Second,
-		WriteTimeout:   200 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-	log.Fatal(s.ListenAndServe())
+	n.Run(":5400")
 }
